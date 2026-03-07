80c813896a6badbf33e1a97677bb442730b98053
Kalp Soni <kalpsoni@Kalps-MacBook-Pro.local>
2026-03-07 18:26:23 -0500

    Enable remote state backend for all environments

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,8 @@
-# Local backend for quick demos. Switch to remote for team use:
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "terraform-state-rg"
-#     storage_account_name = "tfstatemlops3tier"
-#     container_name       = "tfstate"
-#     key                  = "dev/terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstatemlops3tier"
+    container_name       = "tfstate"
+    key                  = "dev/terraform.tfstate"
+  }
+}
