a41431ed5482ce788d1a8abf5fda7e961f819525
Kalp Soni <kalpsoni@Kalps-MacBook-Pro.local>
2026-03-07 22:45:21 -0500

    Fix: make tenant_id optional in keyvault module

--- modules/keyvault/variables.tf
+++ modules/keyvault/variables.tf
@@ -1,35 +1,61 @@
 variable "resource_group_name" {
   type = string
 }
+
 variable "location" {
   type = string
 }
+
 variable "environment" {
-  type = string
+  type    = string
+  default = ""
 }
+
+variable "prefix" {
+  type    = string
+  default = ""
+}
+
+variable "suffix" {
+  type    = string
+  default = ""
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
 variable "tenant_id" {
-  type = string
+  type    = string
+  default = ""
 }
+
 variable "aks_identity_id" {
   type    = string
   default = ""
 }
+
 variable "aks_csi_identity_id" {
   type    = string
   default = ""
 }
+
 variable "aks_kubelet_identity_id" {
   type    = string
   default = ""
 }
+
 variable "db_username" {
   type    = string
   default = "pgadmin"
 }
+
 variable "db_name" {
   type    = string
   default = "appdb"
 }
+
 variable "db_fqdn" {
   type    = string
   default = ""
