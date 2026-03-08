7574e357c55e59912b6d908aeb9b61f4ab915e9b
Kalp Soni <kalpsoni@Kalps-MacBook-Pro.local>
2026-03-07 22:41:57 -0500

    Fix: force clean acr variables.tf

--- modules/acr/variables.tf
+++ modules/acr/variables.tf
@@ -1,25 +1,18 @@
 variable "resource_group_name" {
   type = string
 }
+
 variable "location" {
   type = string
 }
+
 variable "environment" {
   type = string
 }
+
 variable "sku" {
   type    = string
   default = "Basic"
-}
-
-variable "suffix" {
-  type    = string
-  default = ""
-}
-
-variable "tags" {
-  type    = map(string)
-  default = {}
 }
 
 variable "suffix" {
